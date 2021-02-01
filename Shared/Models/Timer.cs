cd0b4b4305f5e3cb7e58201b6d5d40b456959616
Mary Sturgeon <maryannesturgeon@gmail.com>
2021-02-01 13:46:36 -0800

    Initial Commit

---
+++ Shared/Models/Timer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared.Models
+{
+    public class Timer
+    {
+    private double attend {get; set; }
+    private int meetings {get; set; }
+    private double cost {get; set; }
+    }
+}
